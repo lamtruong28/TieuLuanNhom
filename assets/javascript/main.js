1de6216db7da409bffbcf7028c94cb58a1e22863
lamtruong28 <lamtruongpk31@gmail.com>
2021-09-23 21:20:09 +0700

    update file

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,12 +11,10 @@ var btnDecreases = $$(".decrease"); //lấy ds nút giảm
 var boxQuantitys = $$(".quantity"); //lấy ds box số lượng hiển thị
 var btnIncreases = $$(".increase"); //lấy ds nút tăng
 var btnMovePages = $$(".move-pages__btn"); //ds nút chuyển trang;
-// var btnMovePagesDrinks = $$(".drink-list .move-pages__btn");
-var foodPageMenues = $$(".food .page-menu");
-var drinkPageMenues = $$(".drink .page-menu");
-var btnBackPage = $('.page__btn-back');
-var btnNextPage = $('.page__btn-next');
-
+var foodPageMenues = $$(".food .page-menu"); //ds trang đồ ăn;
+var drinkPageMenues = $$(".drink .page-menu"); //ds trang đồ uống
+var btnBackPage = $('.page__btn-back'); //nút lùi trang
+var btnNextPage = $('.page__btn-next'); //nút tiến trang
 
 
 //Hàm hiển thị modal;
@@ -42,41 +40,86 @@ containerOderForm.addEventListener("click", function(event){
     event.stopPropagation();
 });
 
-//Hàm hiển thị danh sách đồ uống
+//Chuyển menu"
 btnMenues.forEach(function(btnMenu, index) {
     var menuItem = menuLists[index];
+
     btnMenu.addEventListener('click', function() {
         $('.btn-menu.active').classList.remove('active');
         this.classList.add('active');
+
         $('.menu-lists.menu-current').style.animation = 'showHidden linear .2s';
+        menuItem.style.animation = 'hiddenShow linear .2s';
         setTimeout(function(){
             $('.menu-lists.menu-current').classList.remove('menu-current');
             menuItem.classList.add('menu-current');
         },200);
-        menuItem.style.animation = 'hiddenShow linear .2s';
     })
 });
 
-//JS chuyển trang;
-btnMovePages.forEach(function(btnMovePageFood , index){
-    btnMovePageFood.addEventListener('click', function() {
-        var foodPageMenue = foodPageMenues[index];
-        var drinkPageMenue = drinkPageMenues[index];
+// JS chuyển trang;
+btnMovePages.forEach(function(btnMovePage , index){
+    btnMovePage.addEventListener('click', function() {
+        var foodPageMenu = foodPageMenues[index];
+        var drinkPageMenu = drinkPageMenues[index];
+
         $('.move-pages__btn.active').classList.remove("active"); //Kiểm tra nếu nút đang active thì bỏ active;
         this.classList.add("active");
+
         $('.food .page-menu.page-current').classList.remove('page-current');
-        foodPageMenue.classList.add('page-current');
+        foodPageMenu.classList.add('page-current');
+
         $('.drink .page-menu.page-current').classList.remove('page-current');
-        drinkPageMenue.classList.add('page-current');
+        drinkPageMenu.classList.add('page-current');
         
-        if(index > 0) {
+        i = j = index + 1;
+        x = y = index - 1;
+        if(index > 0)
             btnBackPage.classList.remove('disable');
-        }else {
+        else 
             btnBackPage.classList.add('disable');
-        }
-    })
+
+        if(i == 4)
+            btnNextPage.classList.add('disable');
+        else
+            btnNextPage.classList.remove('disable');
+    });
 });
 
+// js nút tới:
+var i=1,j=1;
+btnNextPage.addEventListener('click', function() {
+    x=y=i-1;
+    if(i < 4){
+        $('.move-pages__btn.active').classList.remove("active");
+        btnMovePages[i++].classList.add("active");
+        $('.food .page-menu.page-current').classList.remove('page-current');
+        foodPageMenues[j++].classList.add('page-current');
+        btnBackPage.classList.remove('disable');
+        if(i == 4)
+            btnNextPage.classList.add('disable');
+        else
+            btnNextPage.classList.remove('disable');
+    }
+});
+
+// JS nút lùi trang:
+var x,y;
+btnBackPage.addEventListener('click', function() {
+    i=j=x+1;
+    if(x >= 0){
+        $('.move-pages__btn.active').classList.remove("active");
+        btnMovePages[x--].classList.add("active");
+        $('.food .page-menu.page-current').classList.remove('page-current');
+        foodPageMenues[y--].classList.add('page-current');
+        btnBackPage.classList.remove('disable');
+        if(x == 0)
+            btnBackPage.classList.add('disable');
+        else
+            btnBackPage.classList.remove('disable');
+            btnNextPage.classList.remove('disable');
+    }
+});
 
 
 
@@ -88,7 +131,8 @@ for (let i = 0; i < btnIncreases.length ; i++) {
     btnIncrease.addEventListener('click', function() {
         if(boxQuantitys[i].value < MAX)
             boxQuantitys[i].value++;
-        else
+        
+        if(boxQuantitys[i].value == MAX)
             btnIncreases[i].classList.add('disable');
         btnDecreases[i].classList.remove('disable');
     })
@@ -100,8 +144,9 @@ for (let i = 0; i < btnDecreases.length ; i++) {
     btnDecrease.addEventListener('click', function() {
         if(boxQuantitys[i].value > 0)
             boxQuantitys[i].value--;
-        else
-        btnDecreases[i].classList.add('disable');
+
+        if(boxQuantitys[i].value==0)
+            btnDecreases[i].classList.add('disable');
         btnIncreases[i].classList.remove('disable');
     }) 
 }
